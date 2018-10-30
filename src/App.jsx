ba0d85ca42a9eb394a8695e7f34acf229aa35c5c
carlocadiz <carlocadiz71@gmail.com>
2018-10-30 02:51:24 +0000

    initialize project and converted html into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
+import Chatbar from "./ChatBar.jsx";
+import Message from "./Message.jsx";
 
 class App extends Component {
   render() {
+
     return (
-      <h1>Hello React :)</h1>
+     <div>
+     <nav className="navbar">
+     <a href="/" className="navbar-brand">Chatty</a>
+     </nav>
+     <Message/>
+     <Chatbar/>
+     </div>
     );
-  }
+  };
 }
+
+
 export default App;
